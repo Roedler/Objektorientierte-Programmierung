1e456f8680714a1dfc04a61b4b4066d49e0b8fd4
LennZone <lennart.novak.abtei@gmail.com>
2025-05-26 19:06:14 +0200

    refactor(praktikum_6): change array brackets to dots

--- Praktikum/Praktikum_6/de/hsruhrwest/oop/ss2025/assignment6/Aufgabe_2/AbstractStringTransformer.java
+++ Praktikum/Praktikum_6/de/hsruhrwest/oop/ss2025/assignment6/Aufgabe_2/AbstractStringTransformer.java
@@ -2,7 +2,7 @@ package Praktikum.Praktikum_6.de.hsruhrwest.oop.ss2025.assignment6.Aufgabe_2;
 
 public abstract class AbstractStringTransformer {
 
-    public void transform(String[] strings) {
+    public void transform(String... strings) {
         for (int i = 0; i < strings.length; i++) {
             strings[i] = transformString(strings[i]);
         }
