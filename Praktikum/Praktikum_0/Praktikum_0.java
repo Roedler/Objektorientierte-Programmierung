9370280e2ff025c962c2692b88f05a1f061627a0
LennZone <lennart.novak.abtei@gmail.com>
2025-06-23 17:57:41 +0200

    refactor(praktikum_0): file structure

---
+++ Praktikum/Praktikum_0/Praktikum_0.java
@@ -0,0 +1,11 @@
+package Praktikum.Praktikum_0;
+
+public class Praktikum_0 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                OneOhOne.outputProduct(i, j);
+            }
+        }
+    }
+}
